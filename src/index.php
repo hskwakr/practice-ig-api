b69d3928ac8b96f2134bea6ec6c59c490371f1bf
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-01-25 15:51:40 +0900

    Replace funtions in index.php with methods
    Replace funtions in index.php with methods in Ig_Api class

--- src/index.php
+++ src/index.php
@@ -27,86 +27,6 @@ define('FB_API_BASE', 'https://graph.facebook.com/v12.0');
 //*************************************
 // Function
 //*************************************
-function sendRequest($query)
-{
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $query);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    return json_decode($response);
-}
-
-// For DEBUG
-function printJson($json)
-{
-    echo '<pre>';
-    echo json_encode($json, JSON_PRETTY_PRINT);
-    echo '</pre>';
-}
-
-function getUserPages()
-{
-    $endpoint = '/me/accounts?';
-    $options =
-        'access_token=' . APP_ACCESS_TOKEN;
-
-    $query = FB_API_BASE . $endpoint . $options;
-    //echo $query;
-    return sendRequest($query);
-}
-
-function getIgUser($pageId)
-{
-    $endpoint = '/' . $pageId . '?';
-    $options =
-        'access_token=' . APP_ACCESS_TOKEN .
-        '&fields=instagram_business_account';
-
-    $query = FB_API_BASE . $endpoint . $options;
-    //echo $query;
-    return sendRequest($query);
-}
-
-function getIgMedia($userId)
-{
-    $endpoint = '/' . $userId . '/media?';
-    $options =
-        'access_token=' . APP_ACCESS_TOKEN;
-
-    $query = FB_API_BASE . $endpoint . $options;
-    //echo $query;
-    return sendRequest($query);
-}
-
-
-function searchHashtag($userId, $hashtag)
-{
-    $endpoint = '/ig_hashtag_search?';
-    $options =
-        'access_token=' . APP_ACCESS_TOKEN .
-        '&user_id=' . $userId .
-        '&q=' . $hashtag;
-
-    $query = FB_API_BASE . $endpoint . $options;
-    //echo $query;
-    return sendRequest($query);
-}
-
-function getRecentMediasByHashtag($userId, $hashtagId)
-{
-    $endpoint = '/' . $hashtagId . '/recent_media?';
-    $options =
-        'access_token=' . APP_ACCESS_TOKEN .
-        '&user_id=' . $userId .
-        '&fields=media_type,media_url,permalink';
-
-    $query = FB_API_BASE . $endpoint . $options;
-    //echo $query;
-    return sendRequest($query);
-}
 
 //*************************************
 // Main
@@ -115,34 +35,30 @@ function getRecentMediasByHashtag($userId, $hashtagId)
 // Initialize Ig api
 $api = new Ig_Api(new Http_Client(), APP_ACCESS_TOKEN);
 
-// get the user's pages
-$userPages = getUserPages();
-//printJson($userPages);
+$pages_id = $api->getUserPagesId();
+if (isset($pages_id->error)) {
+    $api->printJson($pages_id);
+} else {
+    //echo $pages_id;
+    $user_id = $api->getIgUserId($pages_id);
 
-// capture the page id
-$pageId = $userPages->data[0]->id;
-//echo $pageId;
+    if (isset($user_id->error)) {
+        $api->printJson($user_id);
+    } else {
+        //echo $user_id;
+        $hashtag_id = $api->searchHashtagId($user_id, 'b3d');
 
-// get the page's instagram business account
-$igUser = getIgUser($pageId);
-//printJson($igUser);
+        if (isset($hashtag_id->error)) {
+            $api->printJson($hashtag_id);
+        } else {
+            //echo $hashtag_id;
+            $medias = $api->getRecentMediasByHashtag($user_id, $hashtag_id);
 
-// capture the connected ig user id
-$igUserId = $igUser->instagram_business_account->id;
-//echo $igUserId;
-
-// get the instagram business account's media objects
-//$igMedias = getIgMedia($igUserId);
-//printJson($igMedias);
-
-// search the post by hashtag name
-$igHashtagContext = searchHashtag($igUserId, 'b3d');
-//printJson($igHashtagContext);
-
-// capture the hashtag id
-$igHashtagId = $igHashtagContext->data[0]->id;
-//echo $igHashtagId;
-
-// get the recent medias by hashtag id
-$igMedias = getRecentMediasByHashtag($igUserId, $igHashtagId);
-printJson($igMedias);
+            if (isset($medias->error)) {
+                $api->printJson($medias);
+            } else {
+                $api->printJson($medias);
+            }
+        }
+    }
+}
